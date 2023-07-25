81eb091137cbb52d2b26d01b0f3a710302ebcb65
jiminc77 <jiminc777@gmail.com>
2023-07-25 17:47:12 +0900

    commit

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,7 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-// const uri = process.env.MONGODB_URI as string; // your mongodb connection string
-const uri = 'mongodb+srv://sowhen:djswpqhfRk@cluster0.bmruahw.mongodb.net/'; // your mongodb connection string
+const uri = process.env.MONGODB_URI as string; // your mongodb connection string
 const options = {};
 
 declare global {
