6c7f1ff823ae0fc5fd3bfce92530cbc83e7aec7d
jiminc77 <jiminc777@gmail.com>
2023-07-26 17:33:29 +0900

    commit

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -3,26 +3,43 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 export default function CalendarGfg() {
-    const [value, setValue] = useState<Date>(new Date());
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const [value, setValue] = useState<Date | null>(null);
 
     const onClickDay: (value: Date) => void = (value) => {
         setValue(value);
     };
 
-    const tileClassName = ({ date }: { date: Date }) => {
-        if (value instanceof Date && date.getDate() === value.getDate() && date.getMonth() === value.getMonth() && date.getFullYear() === value.getFullYear()) {
+    const tileClassName = ({ date, view }: { date: Date, view: string }) => {
+        if (
+            view === 'month' && 
+            value instanceof Date && 
+            date.getDate() === value.getDate() && 
+            date.getMonth() === value.getMonth() && 
+            date.getFullYear() === value.getFullYear() && 
+            date.getTime() !== today.getTime()
+        ) {
             return 'highlight';
         }
     }
 
+    const tileDisabled = ({ date }: { date: Date }) => {
+        return date < today;
+    };
+
     return (
         <div>
-            <h1>NextJs Calendar - GeeksforGeeks</h1>
+            <h1>날짜를 선택해주세요</h1>
             <Calendar
                 onClickDay={onClickDay}
                 tileClassName={tileClassName}
+                tileDisabled={tileDisabled}
                 value={value}
             />
         </div>
     );
 }
+
+
