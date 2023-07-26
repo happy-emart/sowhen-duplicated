47422190d96694f5df30ca9ce4e9984e592518f0
jiminc77 <jiminc777@gmail.com>
2023-07-26 12:12:29 +0900

    commit

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,53 +1,49 @@
 import { signIn } from 'next-auth/react';
 import { useState } from 'react';
 import { LoadingDots } from '@/components/icons';
-import Navbar from '@/components/layout/navbar';
 
 export default function LoginPage() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <>
-      <Navbar setSidebarOpen={() => {}} hideLoginButton />
-      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              로그인
-            </h2>
-          </div>
-          <div>
-            <button
-              disabled={loading}
-              onClick={() => {
-                setLoading(true);
-                signIn('github', { callbackUrl: `/` });
-              }}
-              className={`${
-                loading
-                  ? 'bg-gray-200 border-gray-300'
-                  : 'bg-black hover:bg-white border-black'
-              } w-full h-8 py-2 text-white hover:text-black border rounded-md text-sm transition-all`}
-            >
-              {loading ? <LoadingDots color="gray" /> : 'GitHub으로 로그인'}
-            </button>
-            <button
-              disabled={loading}
-              onClick={() => {
-                setLoading(true);
-                signIn('google', { callbackUrl: `/` });
-              }}
-              className={`${
-                loading
-                  ? 'bg-gray-200 border-gray-300'
-                  : 'bg-red-600 hover:bg-white border-red-600'
-              } w-full h-8 py-2 text-white hover:text-black border rounded-md text-sm transition-all mt-4`}
-            >
-              {loading ? <LoadingDots color="gray" /> : 'Google로 로그인'}
-            </button>
-          </div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            로그인
+          </h2>
+        </div>
+        <div>
+          <button
+            disabled={loading}
+            onClick={() => {
+              setLoading(true);
+              signIn('github', { callbackUrl: `/` });
+            }}
+            className={`${
+              loading
+                ? 'bg-gray-200 border-gray-300'
+                : 'bg-black hover:bg-white border-black'
+            } w-full h-8 py-2 text-white hover:text-black border rounded-md text-sm transition-all`}
+          >
+            {loading ? <LoadingDots color="gray" /> : 'GitHub으로 로그인'}
+          </button>
+          <button
+            disabled={loading}
+            onClick={() => {
+              setLoading(true);
+              signIn('google', { callbackUrl: `/` });
+            }}
+            className={`${
+              loading
+                ? 'bg-gray-200 border-gray-300'
+                : 'bg-red-600 hover:bg-white border-red-600'
+            } w-full h-8 py-2 text-white hover:text-black border rounded-md text-sm transition-all mt-4`}
+          >
+            {loading ? <LoadingDots color="gray" /> : 'Google로 로그인'}
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
