86eb8a6b7e2c6c9a69e1b83e38e151391b10165d
jiminc77 <jiminc777@gmail.com>
2023-07-27 20:10:36 +0900

    commit

--- components/appointment.tsx
+++ components/appointment.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Button, Card, CardContent, Typography } from '@material-ui/core';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export interface AppointmentProps {
     appointments: {
@@ -32,6 +34,13 @@ export interface AppointmentProps {
       setCurrentAppointments(currentAppointments.map(appointment => appointment._id === _id ? { ...appointment, isAccepted: true } : appointment));
     
       console.log('Appointment accepted');
+      toast.success("Appointment accepted", {
+        autoClose: 3000, // Duration of the toast in milliseconds (e.g., 3000 ms = 3 seconds)
+        hideProgressBar: true, // Hide the progress bar
+        style: {
+          backgroundColor: '#333', // Set the background color of the toast
+          },
+        });
     }
     
     const handleReject = async (_id: string) => {
@@ -44,6 +53,14 @@ export interface AppointmentProps {
       });
     
       setCurrentAppointments(currentAppointments.filter(appointment => appointment._id !== _id));
+      toast.success("Reject Successful", {
+        autoClose: 3000, // Duration of the toast in milliseconds (e.g., 3000 ms = 3 seconds)
+        hideProgressBar: true, // Hide the progress bar
+        style: {
+          backgroundColor: '#333', // Set the background color of the toast
+          },
+        });
+      
     }
   
     return (
@@ -51,11 +68,11 @@ export interface AppointmentProps {
           {currentAppointments && currentAppointments.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()).map((appointment, index) => (
               <Card key={index} style={{ margin: '1em' }}>
                   <CardContent>
-                      <Typography variant="h6">Sender: {appointment.senderId}</Typography>
-                      <Typography variant="body2">Date: {appointment.date}</Typography>
-                      <Typography variant="body2">Start time: {appointment.startTime}</Typography>
-                      <Typography variant="body2">End time: {appointment.endTime}</Typography>
-                      <Typography variant="body2">Is accepted: {appointment.isAccepted.toString()}</Typography>
+                      <Typography variant="h6">보낸이: {appointment.senderId}</Typography>
+                      <Typography variant="body2">약속 일자: {appointment.date}</Typography>
+                      <Typography variant="body2">약속 시작 시간: {appointment.startTime}</Typography>
+                      <Typography variant="body2">약속 종료 시간: {appointment.endTime}</Typography>
+                      <Typography variant="body2">수락 여부: {appointment.isAccepted.toString()}</Typography>
                       {!appointment.isAccepted && (
                           <>
                               <Button variant="contained" color="primary" onClick={() => handleAccept(appointment._id)}>Accept</Button>
